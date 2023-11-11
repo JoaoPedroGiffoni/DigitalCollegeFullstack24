33290b35f7fe5ae0685fbde8c9103498c9e7448b
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-11 17:00:27 -0300

    Atualiza Aula

--- 20231111/script.js
+++ 20231111/script.js
@@ -1,3 +1,4 @@
+//FUNÇÕES
 // let saudacao = "João Pedro Giffoni"
 // alert(saudacao.charAt(0)+saudacao.charAt(11))
 
@@ -26,9 +27,28 @@
 // let ladoDireito=nome.slice(1)
 // alert(ladoEsquerdo.toUpperCase()+ ladoDireito.toLocaleLowerCase())
 
-let nome = "joão"
-let nome2 = nome.charAt(0)
-let nome3 = nome2.toUpperCase()
-let nome4 = nome.slice(1)
-let nome5 = nome3 + nome4
-alert(nome5)
+// let nome = "joão"
+// let nome2 = nome.charAt(0)
+// let nome3 = nome2.toUpperCase()
+// let nome4 = nome.slice(1)
+// let nome5 = nome3 + nome4
+// alert(nome5)
+
+//Array
+//lista vazia
+// let frutas = []
+//adiconar a lista
+// let frutas = ["Abacaxi", "Banana", "Cacau", "Damasco"]
+
+// frutas.push("Figo")
+
+// alert(frutas)
+
+
+let comida = ["Arroz", " Macarrão ", " Feijão"];
+
+comida.push(" Figo"," Carne "," Pão ")//adiona no ultimo
+
+//escreve no html
+// document.write(comida)
+// document.write(comida[0])
