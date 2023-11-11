f6ba2208cfd8cf4bf40f55b5421c7a2d37df6917
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-11 15:40:19 -0300

    Atualiza Aula

--- 20231111/script.js
+++ 20231111/script.js
@@ -1,3 +1,34 @@
-let saudacao = "João Pedro Giffoni"
+// let saudacao = "João Pedro Giffoni"
+// alert(saudacao.charAt(0)+saudacao.charAt(11))
 
-alert(saudacao.charAt(0)+saudacao.charAt(11))
+// let nome = "João"
+// let sobrenome = "Giffoni"
+// alert(nome.charAt() + sobrenome.charAt())
+
+// let nome = "João"
+// let sobrenome = "Giffoni"
+// let iniciais = nome.charAt()+sobrenome.charAt()
+// alert(iniciais)
+
+// let nome = "João"
+// let sobrenome = "Giffoni"
+// let iniciais = nome.charAt()+sobrenome.charAt()
+// alert(nome.replace("o","B"))
+
+// let nome = "João"
+// alert(nome.replace("J","B"))
+
+// let nome = "João"
+// alert(nome.toUpperCase())
+
+// let nome = "joão"
+// let ladoEsquerdo=nome.slice(0,1);
+// let ladoDireito=nome.slice(1)
+// alert(ladoEsquerdo.toUpperCase()+ ladoDireito.toLocaleLowerCase())
+
+let nome = "joão"
+let nome2 = nome.charAt(0)
+let nome3 = nome2.toUpperCase()
+let nome4 = nome.slice(1)
+let nome5 = nome3 + nome4
+alert(nome5)
