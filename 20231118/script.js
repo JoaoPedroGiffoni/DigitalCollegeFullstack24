9cbf3abac234361eb1b065f6d109bb5825339804
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-18 15:30:27 -0300

    181123

--- 20231118/script.js
+++ 20231118/script.js
@@ -16,20 +16,52 @@
 // alert("Boa tarde, "+pessoa.sobrenome+" voce tem: "+pessoa.idade+" anos.")
 
 
-let gato = {
-    nome: "Gato",
-    peso: 2,
-    raca: "Siames",
+// let gato = {
+//     nome: "Gato",
+//     peso: 2,
+//     raca: "Siames",
 
-    miar(){
-        return "miauuuu"
-    },
+//     miar() {
+//         return "miauuuu"
+//     },
 
-    toString(){
-        return this.nome +", "+ this.raca
-    }
+//     toString() {
+//         return this.nome + ", " + this.raca
+//     }
+// }
+
+// alert(gato.toString())
+
+
+// let gato = {
+//     nome: "Gato",
+//     raca: "Siames",
+// }
+
+// let gato1 = {
+//     nome: "Lico",
+//     raca: "Persa",
+// }
+
+// let gato2 = {
+//     nome: "Juba",
+// }
+
+// alert(gato1.raca)
+
+
+
+
+function Gato(nomePassado, racaPassada) {
+    this.nome = nomePassado;
+    this.raca = racaPassada
+
 }
 
-alert(gato.toString())
+let gato = new Gato("Lico", "Sianes")
 
+let gato1 = new Gato ("Lico","Persa")
 
+let gato2 = new Gato("Juba")
+
+console.log(gato,gato1,gato2)
