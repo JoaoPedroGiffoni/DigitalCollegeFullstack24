1da9d441170a5ff286379fd9b890d3779fb2cc0a
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-18 14:22:50 -0300

    181123

---
+++ 20231118/script.js
@@ -0,0 +1,15 @@
+// let carro = {
+//     marca:"Fiat",
+//     preco:28.789,
+//     modelos:"Mobi"
+// }
+
+// alert(carro.marca)
+
+let pessoa ={
+    nome:"João Pedro",
+    sobrenome:"Giffoni",
+    idade: 30
+}
+
+alert("Boa tarde, "+pessoa.sobrenome+" voce tem: "+pessoa.idade+" anos.")
