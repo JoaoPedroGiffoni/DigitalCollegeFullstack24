f164f8761b95d94cf7a46f6c58de5e6620a4922e
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-18 16:01:17 -0300

    181123

--- 20231118/script.js
+++ 20231118/script.js
@@ -50,9 +50,54 @@
 // alert(gato1.raca)
 
 
+// function Gato(nomePassado, racaPassada) {
+//     this.nome = nomePassado;
+//     this.raca = racaPassada
+
+// }
+
+// let gato = new Gato("Lico", "Sianes")
+
+// let gato1 = new Gato ("Lico","Persa")
+
+// let gato2 = new Gato("Juba")
+
+// console.log(gato2)
 
 
-function Gato(nomePassado, racaPassada) {
+// function Gato(nomePassado, racaPassada) {
+//     console.log(nomePassado, racaPassada)
+//     this.nome = nomePassado;
+//     this.raca = racaPassada
+
+// }
+
+// let gato = new Gato("Lico", "Sianes")
+
+// let gato1 = new Gato ("Lico","Persa")
+
+// let gato2 = new Gato("Juba")
+
+
+// function Gato(nomePassado = "", racaPassada = "") {
+
+//     // console.log(nomePassado, racaPassada)
+//     this.nome = nomePassado;
+//     this.raca = racaPassada
+
+// }
+
+// let gato = new Gato("Lico", "Sianes")
+
+// let gato1 = new Gato("Lico", "Persa")
+
+// let gato2 = new Gato("Juba")
+
+// console.log(gato.raca.toUpperCase())
+
+
+function Gato(nomePassado = "", racaPassada = "") {
+
     this.nome = nomePassado;
     this.raca = racaPassada
 
@@ -60,8 +105,15 @@ function Gato(nomePassado, racaPassada) {
 
 let gato = new Gato("Lico", "Sianes")
 
-let gato1 = new Gato ("Lico","Persa")
+let gato1 = new Gato("Lico", "Persa")
 
 let gato2 = new Gato("Juba")
 
-console.log(gato,gato1,gato2)
+// console.log(gato.raca.toUpperCase())
+
+let nome = new String("João Pedro")
+let idade = new Number(30)
+let teste = new Boolean("10")
+let cores = new Array("Vermelho","azul")
+
+console.log(cores)
