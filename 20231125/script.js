7d3394d47936e64deec6ebacd148fdbb0ee21c3b
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-11-25 16:49:21 -0300

    25112023

--- 20231125/script.js
+++ 20231125/script.js
@@ -1,54 +1,54 @@
-// Achar o elemento no html
-// const h1Selecionado = document.querySelector("h1");
+Achar o elemento no html
+const h1Selecionado = document.querySelector("h1");
 
-// console.log(h1Selecionado);
+console.log(h1Selecionado);
 
-// // Manipular o html pelo js
-// const h1Selecionado = document.querySelector("h1");
-
-
-// h1Selecionado.style.background = "red";
-// h1Selecionado.textContent="FEC"; troca o texto
-// h1Selecionado.innerHTML="FEC"; Adionar um codigo html
-
-// console.log(h1Selecionado);
-
-// const h1Selecionado = document.querySelector("h1");
-// h1Selecionado.style
+// Manipular o html pelo js
+const h1Selecionado = document.querySelector("h1");
 
 
-// h1Selecionado.innerText = new Date().toLocaleDateString("pr-br");
+h1Selecionado.style.background = "red";
+h1Selecionado.textContent="FEC"; troca o texto
+h1Selecionado.innerHTML="FEC"; Adionar um codigo html
 
-// TRocar cor apertando no botão
-// const divselecionadadia = document.querySelector("div");
-// function divselecionad(){
+console.log(h1Selecionado);
 
-//     divselecionadadia.style.background = "yellow"
-// }
-
-// const divselecionadanoite = document.querySelector("div");
-// function divselecionan(){
-
-//     divselecionadanoite.style.background = "black"
-// }
-
-// Outra forma
-
-// function divselecionad(){
-//     const divselecionadadia = document.getElementById("circulo");
-//     divselecionadadia.style.background = "yellow"
-// }
+const h1Selecionado = document.querySelector("h1");
+h1Selecionado.style
 
 
-// function divselecionan(){
-//     const divselecionadanoite = document.getElementById("circulo");
-//     divselecionadanoite.style.background = "black"
-// }
-// const circulo = document.getElementById("ciruculo")
+h1Selecionado.innerText = new Date().toLocaleDateString("pr-br");
 
-// function mudar(){
-//     circulo.classList.toggle("noite");
-// }
+TRocar cor apertando no botão
+const divselecionadadia = document.querySelector("div");
+function divselecionad(){
+
+    divselecionadadia.style.background = "yellow"
+}
+
+const divselecionadanoite = document.querySelector("div");
+function divselecionan(){
+
+    divselecionadanoite.style.background = "black"
+}
+
+Outra forma
+
+function divselecionad(){
+    const divselecionadadia = document.getElementById("circulo");
+    divselecionadadia.style.background = "yellow"
+}
+
+
+function divselecionan(){
+    const divselecionadanoite = document.getElementById("circulo");
+    divselecionadanoite.style.background = "black"
+}
+const circulo = document.getElementById("ciruculo")
+
+function mudar(){
+    circulo.classList.toggle("noite");
+}
 
 const botoes = document.getElementsByTagName("button")
 
