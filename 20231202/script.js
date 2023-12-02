9b502fb52fd6e71da94d86f6fc75141195358a10
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-12-02 14:56:25 -0300

    Aula 02.12.23

---
+++ 20231202/script.js
@@ -0,0 +1,38 @@
+function pegarValorInput() {
+    const entrada = document.getElementById("entrada");
+
+    return entrada.value;
+}
+
+function apagarValorInput() {
+    const entrada = document.getElementById("entrada");
+
+    entrada.value = "";
+}
+
+function pegarLista() {
+    const lista = document.getElementById("lista");
+
+    return lista;
+}
+
+function criarElementoLI(valor) {
+    const li = document.createElement("li");
+    li.onclick =
+        li.textContent = valor;
+
+    return li;
+}
+
+function inserirItem() {
+    const valor = pegarValorInput();
+    const lista = pegarLista();
+
+    if (valor.trim()) {
+        const li = criarElementoLI(valor);
+        lista.appendChild(li);
+        apagarValorInput();
+    } else {
+        alert("Descreva o novo item");
+    }
+}
