02a1da03580a551621343c309bfcc6eeb3a64702
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2023-12-16 15:41:44 -0300

    home

--- 20231216/script.js
+++ 20231216/script.js
@@ -1,5 +1,26 @@
-const retorno = fetch("https://imersao.ronierlima.dev/index.html")
+const url =
+    "https://servicodados.ibge.gov.br/api/v1/localidades/estados/ce/municipios";
 
-retorno.then(() => {
-    alert("Deu bom")
-});
+function criarEAddElemento(cidade) {
+    const h1 = document.createElement("li");
+    h1.textContent = cidade.nome;
+
+    document.body.appendChild(h1);
+}
+
+function buscarCidadesCE() {
+    //retorna uma Promisse
+    const retorno = fetch(url);
+
+    // Then trata a Promisse
+    retorno
+        .then((resposta) => {
+            // transforma a resposta em json, retorna uma nova Promisse
+            return resposta.json();
+        })
+        .then((json) => {
+            json.forEach(criarEAddElemento);
+        });
+}
+
+buscarCidadesCE();
