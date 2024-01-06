5217f051fc8bc691142569d5e0d0025271440a48
JoaoPedroGiffoni <joaopedrogiffoni@hotmail.com>
2024-01-06 16:47:17 -0300

    home

---
+++ 20240106/React/aula0/src/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return( <header>
+        <h1>João Pedro</h1>
+        <a href="">Aqui</a>
+    </header>)
+}
+
+export default Header;
---
+++ 20240106/React/aula0/src/Perfil.jsx
@@ -0,0 +1,8 @@
+function Perfil(){
+    return( <div>
+        <h1>novo h1 João Pedro</h1> 
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlR_wmW45otRntMCHaZfIr33BqyRun4XLtZA&usqp=CAU" alt="" />
+    </div>)
+}
+
+export default Perfil;
